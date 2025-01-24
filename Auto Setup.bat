35c006e4e0953aa7bca5d8e2c49cb5c38e2cde6e
QuackWalks <146853327+QuackWalks@users.noreply.github.com>
2025-01-23 18:29:37 -0600

    Update Auto Setup.bat

--- Auto Setup.bat
+++ Auto Setup.bat
@@ -1,5 +1,5 @@
 @echo off
 REM Download the latest version of the script from github
-Powershell.exe Invoke-WebRequest -Uri https://raw.githubusercontent.com/doakyz/done-set-auto-setup/main/done-auto.ps1 -OutFile "./done-auto.ps1"
+Powershell.exe Invoke-WebRequest -Uri https://raw.githubusercontent.com/QuackWalks/done-set-3-auto-setup/refs/heads/main/done-auto.ps1 -OutFile "./done-auto.ps1"
 REM Run the script
 Powershell.exe -ExecutionPolicy Bypass -File "./done-auto.ps1"
